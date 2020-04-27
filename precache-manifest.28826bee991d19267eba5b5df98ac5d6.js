bee09e76eea6e172934250e4a99e1b25a78b2320
Sudita <sahasudita@gmail.com>
2020-04-27 11:32:43 +0530

    Updates

---
+++ precache-manifest.28826bee991d19267eba5b5df98ac5d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a065cd97e38a06a67165",
+    "url": "/calculator-in-react/static/css/main.f7e297ea.chunk.css"
+  },
+  {
+    "revision": "a065cd97e38a06a67165",
+    "url": "/calculator-in-react/static/js/main.cf06fe7e.chunk.js"
+  },
+  {
+    "revision": "4798b9cf26849931c92b",
+    "url": "/calculator-in-react/static/js/runtime~main.11bb2347.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/calculator-in-react/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "9f70efa301211fff955cac5da9dab064",
+    "url": "/calculator-in-react/index.html"
+  }
+];
